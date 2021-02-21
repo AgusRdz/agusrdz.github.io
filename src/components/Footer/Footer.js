49e3387727c23f4a627fd20240a7cf72761f2082
Agustin Espinoza <agustinurdz_@hotmail.com>
2021-02-21 12:44:40 -0600

    fix footer text spacing

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -3,8 +3,9 @@ import React from 'react'
 const Footer = () => {
   return (
     <div className="flex bg-black h-16 place-content-center items-center text-white">
-      Made with <span className="text-red-intense text-2xl">&#9829;</span> using
-      React & Tailwind
+      Made with{'  '}
+      <span className="text-red-intense text-2xl">&#9829;</span>
+      {'  '}using React & Tailwind
     </div>
   )
 }
