9eb8906849d64c743b0874b495157211dd6aeab1
Agustin Espinoza <agustinurdz_@hotmail.com>
2021-07-03 20:30:31 -0500

    hide map on mobile

--- src/components/CodingActivity/CodingActivity.js
+++ src/components/CodingActivity/CodingActivity.js
@@ -34,7 +34,7 @@ const CodingActivity = () => {
       <div className="text-center mb-4">
         <span className="text-2xl">Coding Activity</span>
       </div>
-      <div className="grid grid-cols-2 gap-4 mx-3 mb-4">
+      <div className="grid md:grid-cols-2 gap-4 mx-3 mb-4">
         {/* <figure className="shadow-black dark:shadow-gray">
           <embed src="https://wakatime.com/share/@agusrdz/906c2a54-6764-47e6-9d02-d05d72c4b993.svg"></embed>
         </figure> */}
